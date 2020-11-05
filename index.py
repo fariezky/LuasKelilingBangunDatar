2888f12e9501d5dabcad28264a9650bbdb6fac55
fariezky <39256439+fariezky@users.noreply.github.com>
2020-11-05 14:01:47 +0700

    menambahkan fitur pertanyaan hitung ulang kembali

--- index.py
+++ index.py
@@ -42,79 +42,86 @@ def kel_ling(r):
     k = 2*3.14*r
     return k
 
-menu()
+ulang = 'y'
+while(ulang == 'y'):
 
-pil = int(input("Masukkan pilihan Anda = "))
+    menu()
 
-#Luas Bujur Sangkar
-if pil == 1:
-    print("-------------------------------")
-    print("Anda memilih Luas Bujur Sangkar")
-    print("-------------------------------")
-    s = float(input("Masukkan sisi (cm) = "))
-    print("Luas Bujur Sangkar = %f" %luas_buj_sang(s), "cm2") 
+    pil = int(input("Masukkan pilihan Anda = "))
 
-#Keliling Bujur Sangkar
-elif pil == 2:
-    print("-----------------------------------")
-    print("Anda memilih Keliling Bujur Sangkar")
-    print("-----------------------------------")
-    s = float(input("Masukkan sisi (cm) =  "))
-    print("Keliling Bujur Sangkar = %f" %kel_buj_sang(s), "cm2")
+    #Luas Bujur Sangkar
+    if pil == 1:
+        print("-------------------------------")
+        print("Anda memilih Luas Bujur Sangkar")
+        print("-------------------------------")
+        s = float(input("Masukkan sisi (cm) = "))
+        print("Luas Bujur Sangkar = %f" %luas_buj_sang(s), "cm2") 
 
-#Luas Persegi Panjang
-elif pil == 3:
-    print("---------------------------------")
-    print("Anda memilih Luas Persegi Panjang")
-    print("---------------------------------")
-    p = float(input("Masukkan panjang (cm) =  "))
-    l = float(input("Masukkan lebar (cm) =  "))
-    print("Luas Persegi Panjang = %f" %luas_persegi_pjg(p,l), "cm2")
+    #Keliling Bujur Sangkar
+    elif pil == 2:
+        print("-----------------------------------")
+        print("Anda memilih Keliling Bujur Sangkar")
+        print("-----------------------------------")
+        s = float(input("Masukkan sisi (cm) =  "))
+        print("Keliling Bujur Sangkar = %f" %kel_buj_sang(s), "cm2")
 
-#Keliling Persegi Panjang
-elif pil == 4:
-    print("-------------------------------------")
-    print("Anda memilih Keliling Persegi Panjang")
-    print("-------------------------------------")
-    p = float(input("Masukkan panjang (cm) =  "))
-    l = float(input("Masukkan lebar (cm) =  "))
-    print("Keliling Persegi Panjang = %f" %kel_persegi_pjg(p,l), "cm2")
+    #Luas Persegi Panjang
+    elif pil == 3:
+        print("---------------------------------")
+        print("Anda memilih Luas Persegi Panjang")
+        print("---------------------------------")
+        p = float(input("Masukkan panjang (cm) =  "))
+        l = float(input("Masukkan lebar (cm) =  "))
+        print("Luas Persegi Panjang = %f" %luas_persegi_pjg(p,l), "cm2")
 
-#Luas Segitiga
-elif pil == 5:
-    print("--------------------------")
-    print("Anda memilih Luas Segitiga")
-    print("--------------------------")
-    a = float(input("Masukkan alas (cm) =  "))
-    t = float(input("Masukkan tinggi (cm) =  "))
-    print("Keliling Persegi Panjang = %f" %luas_segitiga(a,t), "cm2")
+    #Keliling Persegi Panjang
+    elif pil == 4:
+        print("-------------------------------------")
+        print("Anda memilih Keliling Persegi Panjang")
+        print("-------------------------------------")
+        p = float(input("Masukkan panjang (cm) =  "))
+        l = float(input("Masukkan lebar (cm) =  "))
+        print("Keliling Persegi Panjang = %f" %kel_persegi_pjg(p,l), "cm2")
 
-#Keliling Segitiga
-elif pil == 6:
-    print("------------------------------")
-    print("Anda memilih Keliling Segitiga")
-    print("------------------------------")
-    a = float(input("Masukkan sisi a (cm) =  "))
-    b = float(input("Masukkan sisi b (cm) =  "))
-    c = float(input("Masukkan sisi c (cm) =  "))
-    print("Luas Segitiga = %f" %kel_segitiga(a,b,c), "cm2")
+    #Luas Segitiga
+    elif pil == 5:
+        print("--------------------------")
+        print("Anda memilih Luas Segitiga")
+        print("--------------------------")
+        a = float(input("Masukkan alas (cm) =  "))
+        t = float(input("Masukkan tinggi (cm) =  "))
+        print("Keliling Persegi Panjang = %f" %luas_segitiga(a,t), "cm2")
 
-#Luas Lingkaran
-elif pil == 7:
-    print("---------------------------")
-    print("Anda memilih Luas Lingkaran")
-    print("---------------------------")
-    r = float(input("Masukkan jari-jari (cm) =  "))
-    print("Luas Lingkaran = %.f" %luas_ling(r), "cm2")
+    #Keliling Segitiga
+    elif pil == 6:
+        print("------------------------------")
+        print("Anda memilih Keliling Segitiga")
+        print("------------------------------")
+        a = float(input("Masukkan sisi a (cm) =  "))
+        b = float(input("Masukkan sisi b (cm) =  "))
+        c = float(input("Masukkan sisi c (cm) =  "))
+        print("Luas Segitiga = %f" %kel_segitiga(a,b,c), "cm2")
 
-#Keliling Lingkaran
-elif pil == 8:
-    print("-------------------------------")
-    print("Anda memilih Keliling Lingkaran")
-    print("-------------------------------")
-    r = float(input("Masukkan jari-jari (cm) =  "))
-    print("Keliling Lingkaran = %.f" %kel_ling(r), "cm2")
+    #Luas Lingkaran
+    elif pil == 7:
+        print("---------------------------")
+        print("Anda memilih Luas Lingkaran")
+        print("---------------------------")
+        r = float(input("Masukkan jari-jari (cm) =  "))
+        print("Luas Lingkaran = %.f" %luas_ling(r), "cm2")
 
-#keluar
-elif pil == 9:
-    ulang = "t"
+    #Keliling Lingkaran
+    elif pil == 8:
+        print("-------------------------------")
+        print("Anda memilih Keliling Lingkaran")
+        print("-------------------------------")
+        r = float(input("Masukkan jari-jari (cm) =  "))
+        print("Keliling Lingkaran = %.f" %kel_ling(r), "cm2")
+
+    #keluar
+    elif pil == 9:
+        ulang = "t"
+
+    ulang = input("Apakah Anda ingin menghitung ulang? [y/t]")
+    if(ulang == 't'):
+        print("\n Keluar")
